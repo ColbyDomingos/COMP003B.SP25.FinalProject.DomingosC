2d6cdcf4058ad716fc50a817416170f553e8334b
ColbyDomingos <colbydomingos@my.whccd.edu>
2025-05-05 21:31:58 -0700

    Added basic db context

---
+++ COMP003B.SP25.FinalProject.DomingosC/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.SP25.FinalProject.DomingosC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Mechanic> Mechanics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BookingMechanic>()
+                .HasKey(bm => new { bm.BookingId, bm.MechanicId });
+        }
+    }
+}
