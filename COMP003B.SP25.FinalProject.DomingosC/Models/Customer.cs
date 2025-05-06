7c15086d08bed3660b3aa6b5dafc1bc84daacf1c
ColbyDomingos <colbydomingos@my.whccd.edu>
2025-05-05 21:34:49 -0700

    added customer model with comments

---
+++ COMP003B.SP25.FinalProject.DomingosC/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.DomingosC.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; } //sets the id fot the customer
+
+        [Required]
+        public string FirstName { get; set; } // requires a first name for the customer
+
+        [Required]
+        public string LastName { get; set; } // requires a last name for the customer
+
+        
+        [Required, EmailAddress] // requires the email adress
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; } //connects a phone number with the customer
+
+        public ICollection<Vehicle> Vehicles { get; set; } //one to one connection with a vehicle 
+    }
+}
+
